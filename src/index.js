20cbad9cd73bac7e25f415e88d475cf3cf525fc0
CarlaHope <118320308+CarlaHope@users.noreply.github.com>
2023-05-25 14:05:37 -0500

    Update index.js

--- src/index.js
+++ src/index.js
@@ -422,6 +422,25 @@ function resetGame(text) {
   padContainer.classList.add("unclickable");
 }
 
+const audioHit = new Audio("https://github.com/gabrielsanchez/erddiagram/blob/main/hit.mp3?raw=true");
+const song = new Audio("https://github.com/gabrielsanchez/erddiagram/blob/main/molesong.mp3?raw=true");
+
+function playAudio(audioObject) {
+  audioObject.play();
+}
+
+function loopAudio(audioObject) {
+  audioObject.loop = true;
+  playAudio(audioObject);
+}
+
+function stopAudio(audioObject) {
+  audioObject.pause();
+}
+
+function play(){
+  playAudio(song);
+}
 
 
 /**
